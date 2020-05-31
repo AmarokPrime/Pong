ce4f11cb20bb6635db01096454a6ea99ec963741
AmarokPrime <66043972+AmarokPrime@users.noreply.github.com>
2020-05-31 00:05:36 -0600

    Ball moves randomly on spawn, added bounce physics to game objects, and moved gameScene location

---
+++ Pong/Assets/Scripts/BallScripts/BallController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveBallOnSpawn();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        speed += 0.5f;
+    }
+
+    private void MoveBallOnSpawn()
+    {
+        var directionChoice = Random.Range(0, 20);
+        var directionX = Random.Range(2, 10);
+        var directionY = Random.Range(2, 10);
+
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        if (directionChoice <= 5)
+        {
+            rigidBody.velocity = new Vector2(directionX, directionY) * speed;
+        }
+        else if (directionChoice > 5 && directionChoice <= 10)
+        {
+            rigidBody.velocity = new Vector2(directionX, directionY) * speed;
+        }
+        else if (directionChoice > 10 && directionChoice <= 15)
+        {
+            rigidBody.velocity = new Vector2(directionX, directionY) * speed;
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(directionX, directionY) * speed;
+        }
+    }
+}
